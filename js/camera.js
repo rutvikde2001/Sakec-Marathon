244277a47a2aec94cc713a56118f03158c0a4296
Rutvik Deshpande <rutvikde2001@gmail.com>
2021-03-08 14:38:30 +0530

    changes before passing to cllg

--- js/camera.js
+++ js/camera.js
@@ -79,7 +79,7 @@
 		
 		thumbnails			: false,
 		
-		time				: 7000,	//milliseconds between the end of the sliding effect and the start of the nex one
+		time				: 5000,	//milliseconds between the end of the sliding effect and the start of the nex one
 		
 		transPeriod			: 1000,	//lenght of the sliding effect in milliseconds
 		
